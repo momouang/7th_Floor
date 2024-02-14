d69537319f62c3b38cb9db2f03c43e3e625463e4
momouang <syes0923momo@gmail.com>
2024-02-14 16:48:33 +0000

    02/14

---
+++ Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.transform != null)
+                {
+                    PrintName(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    public void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+}
