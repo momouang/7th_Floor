16ec165cc420732d6059f2bc34edad8b7779f6c4
LAPTOP-JVP8CGHA\asus <2675249282@qq.com>
2024-04-21 17:06:19 +0100

    E4

--- Assets/Scripts/LongE/LE1.cs
+++ Assets/Scripts/LongE/LE1.cs
@@ -6,23 +6,42 @@ public class LE1 : MonoBehaviour
 {
     public Transform player; // 玩家的Transform组件
     private float offsetX; // 物体与玩家之间的初始相对X轴距离
+    private float maxPlayerX; // 用来记录玩家的X轴最大位置
+
+    public Collider triggerCollider;
+
+    private bool hasTriggered = false;
 
     public bool doorok = false;
 
+    public bool okok = false;
+
     void Start()
     {
-        // 计算并保存初始相对X轴距离
-        offsetX = transform.position.x - player.position.x;
+
     }
 
     void Update()
     {
-        // 更新物体的X轴位置，使其保持与玩家相同的相对X轴距离
-        // Y轴和Z轴位置保持不变
-        if (doorok == false)
+        if (!hasTriggered && triggerCollider.bounds.Contains(player.position))
         {
+            okok = true;
+            offsetX = transform.position.x - player.position.x;
+            // 初始化maxPlayerX为当前玩家的X轴位置
+            maxPlayerX = player.position.x;
+            hasTriggered = true;  // 设置标志为true，表示事件已触发
+        }
 
-            transform.position = new Vector3(player.position.x + offsetX, transform.position.y, transform.position.z);
+        if ((!doorok) && (okok))
+        {
+            // 更新玩家的X轴最大位置
+            if (player.position.x > maxPlayerX)
+            {
+                maxPlayerX = player.position.x;
+            }
+
+            // 物体的X轴位置更新为玩家的X轴最大位置加上初始相对距离
+            transform.position = new Vector3(maxPlayerX + offsetX, transform.position.y, transform.position.z);
         }
     }
 
