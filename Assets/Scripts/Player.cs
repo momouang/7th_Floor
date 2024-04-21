e0ced1de551c075d373218defe5b7fb1075223e9
momouang <syes0923momo@gmail.com>
2024-04-21 13:29:04 +0100

    13:28

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,18 +7,35 @@ public class Player : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.8f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
 
     public AudioSource footStep;
     public bool isPlayedFootstep;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
         if(move.magnitude >=0.1 && !isPlayedFootstep)
         {
