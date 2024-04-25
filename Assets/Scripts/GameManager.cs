a126ce52a21ef6a2d08ba5776a87258242d02041
momouang <syes0923momo@gmail.com>
2024-04-26 00:06:47 +0100

    add pause menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseScene;
+    public GameObject Player;
+    public Transform transitPoint;
+
+    public static bool isPaused;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            pauseScene.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            pauseScene.SetActive(false);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void Continue()
+    {
+        isPaused = false;
+    }
+
+    public void Retry()
+    {
+        isPaused = false;
+        //StartCoroutine(TransitPlayer());
+        SceneManager.LoadScene(0);
+
+    }
+
+    IEnumerator TransitPlayer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Player.transform.position = transitPoint.position;
+    }
+}
